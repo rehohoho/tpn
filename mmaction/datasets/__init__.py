0b339962e30ebce569586c5758756316ab9c73fe
rehohoho <ernie.birdy.ho@gmail.com>
2021-10-27 13:33:12 +0000

    Update gitignore, add RawHdf5Dataset.

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -1,9 +1,10 @@
 from .rawframes_dataset import RawFramesDataset
+from .rawhdf5_dataset import RawHdf5Dataset
 from .utils import get_untrimmed_dataset, get_trimmed_dataset
 from .loader import GroupSampler, DistributedGroupSampler, build_dataloader
 
 __all__ = [
-    'RawFramesDataset',
+    'RawFramesDataset', 'RawHdf5Dataset',
     'get_trimmed_dataset', 'get_untrimmed_dataset',
     'GroupSampler', 'DistributedGroupSampler', 'build_dataloader'
 ]
