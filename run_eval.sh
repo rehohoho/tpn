e0f6f5c43fd5f9bf6914f769e742148ab6eba521
rehohoho <ernie.birdy.ho@gmail.com>
2021-10-28 03:03:28 +0800

    Fix hdf5 config file name.

--- run_eval.sh
+++ run_eval.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2
 
 export PYTHONPATH=$(pwd)
 
-CONFIG_FILE=config_files/sthv2/tsm_tpn.py
+CONFIG_FILE=config_files/sthv2/tsm_tpn_hdf5.py
 CHECKPOINT_FILE=pretrained/sthv2_tpn.pth
 RESULT_FILE=pretrained/sthv2_tpn.pkl
 EVAL_METRICS=top_k_accuracy
-NUM_GPU=1
+NUM_GPU=3
 
-./tools/dist_test_recognizer.sh ${CONFIG_FILE} ${CHECKPOINT_FILE} ${GPU_NUM} \
+./tools/dist_test_recognizer.sh ${CONFIG_FILE} ${CHECKPOINT_FILE} ${NUM_GPU} \
     --out ${RESULT_FILE} \
-    --eval ${EVAL_METRICS} \
     --ignore_cache \
 2>&1 | tee ${RESULT_FILE}.log
 # --fcn_testing
